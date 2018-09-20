ec9f9a9127c29cbb22e0a9df15b7fd79b08d1fc3
chiragjn <jain.chirag925@gmail.com>
2018-09-20 17:57:28 +0530

    Solve day6

---
+++ 2017/solutions/day6_part1.py
@@ -0,0 +1,24 @@
+def main():
+    # naive strategy 
+    states = set()
+    state = list(map(int, input().strip().split()))
+    states.add(tuple(state))
+    count = 0
+    while True:
+        count += 1
+        val = max(state)
+        i = state.index(val)
+        state[i] = 0
+        while val > 0:
+            i = (i + 1) % len(state)
+            state[i] += 1
+            val -= 1
+        
+        if tuple(state) in states:
+            print(count)
+            break
+
+        states.add(tuple(state))
+
+if __name__ == '__main__':
+    main()
---
+++ 2017/solutions/day6_part2.py
@@ -0,0 +1,26 @@
+def main():
+    # naive strategy 
+    states = {}
+    state = list(map(int, input().strip().split()))
+    cycle = 0
+    states[tuple(state)] = cycle
+    count = 0
+    while True:
+        count += 1
+        cycle += 1
+        val = max(state)
+        i = state.index(val)
+        state[i] = 0
+        while val > 0:
+            i = (i + 1) % len(state)
+            state[i] += 1
+            val -= 1
+        
+        if tuple(state) in states:
+            print(cycle - states[tuple(state)])
+            break
+
+        states[tuple(state)] = cycle
+
+if __name__ == '__main__':
+    main()
