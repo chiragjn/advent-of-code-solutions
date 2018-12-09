d83af53d73bf0efd6bad070a34932e359c2bdaee
chiragjn <jain.chirag925@gmail.com>
2018-12-09 16:10:33 +0530

    Day 7 part 1

--- 2018/solutions/day5_part2.py
+++ 2018/solutions/day5_part2.py
@@ -4,7 +4,7 @@ from typing import List
 
 
 def compress(polymer: str) -> List[str]:
-    stack: List[chr] = []
+    stack: List[str] = []
     for c in polymer:
         if stack and stack[-1] != c and stack[-1].lower() == c.lower():
             stack.pop()
